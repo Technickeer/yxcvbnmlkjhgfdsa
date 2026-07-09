c3a61911d63ffba6b5ace1035bebe6c8aeeac244
Technickeer <clemens.anger@posteo.de>
2026-07-09 16:56:32 +0200

    Increase available days ahead for KIT provider

--- auth/providers/kit.py
+++ auth/providers/kit.py
@@ -8,7 +8,7 @@ class KITProvider(SSOProvider):
 
     name = "KIT"
     domain = "kit.edu"
-    available_days_ahead = 3
+    available_days_ahead = 4
 
     def authenticate(self) -> str:
         self.session.headers.pop('x-requested-with', None)
